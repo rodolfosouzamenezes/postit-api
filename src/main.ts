e52a5ab7d83e4f70935efbdc20c6769ff477e6bf
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-08-04 10:18:44 -0300

    update(main): add environment.PORT

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { enviroment } from './environment/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
+  await app.listen(enviroment.PORT || 3000);
 }
 bootstrap();
