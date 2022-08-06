75a6f3f001336079c56ec975677343fd406c8f0a
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-08-06 11:18:45 -0300

    create(note-like): added note like module

--- src/modules/note/note.module.ts
+++ src/modules/note/note.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthTokenModule } from '../auth/auth-token.module';
-import { NoteController } from './controllers/note.controller';
 import { NoteEntity } from './entities/note.entity';
 import { NoteService } from './services/note.service';
 
 @Module({
-  controllers: [NoteController],
-  imports: [TypeOrmModule.forFeature([NoteEntity]), AuthTokenModule],
+  imports: [TypeOrmModule.forFeature([NoteEntity])],
   providers: [NoteService],
   exports: [NoteService],
 })
